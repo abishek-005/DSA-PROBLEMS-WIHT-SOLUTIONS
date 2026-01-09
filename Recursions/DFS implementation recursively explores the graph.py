252b5454d41dd17b72442d8288ab64581c135d50
Abishek kishan <ak.abishek005@gmail.com>
2026-01-09 17:04:18 +0530

    Add files via upload

---
+++ Recursions/DFS implementation recursively explores the graph.py
@@ -0,0 +1,24 @@
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(node)
+    print(node, end=' ')
+
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+
+# Example usage
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+print("DFS traversal:")
+dfs(graph, 'A')
---
+++ Recursions/linked list traversal using recursion.py
@@ -0,0 +1,30 @@
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+def print_list(head):
+    if head:
+        print(head.value, end='')
+        print_list(head.next)
+    else:
+        print() # Print newline at the end
+# Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+print("Linked list:")
+print_list(head)
+print("---------------------")
+print("Reversing the linked list:")
+def reverse_list(head):
+    if not head or not head.next:
+        return head
+    new_head = reverse_list(head.next)
+    head.next.next = head
+    head.next = None
+    return new_head
+# Reverse the list
+new_head = reverse_list(head)
+print_list(new_head)
